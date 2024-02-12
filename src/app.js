5cbcf40b2b99938ae2910d62a0b6f5342af0a88f
bankiiz789 <sukkarin7303@gmail.com>
2024-02-12 19:14:57 +0700

    design database

--- src/app.js
+++ src/app.js
@@ -1,14 +1,16 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const notFound = require("./middleware/not-found");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+app.use(notFound());
+//Server
 const PORT = process.env.PORT || 8000;
-
 app.listen(PORT, () => {
   console.log(` server is running on port ${PORT} `);
 });
---
+++ src/service/jwt-service.js
@@ -0,0 +1,9 @@
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = process.env.JWT_SECRET;
+const EXPIRE_IN = process.env.EXPIRE_IN || "7d";
+
+exports.sign = (payload) => {
+  jwt.sign(payload, JWT_SECRET, { expiresIn: EXPIRE_IN });
+};
+
+exports.verify = (token) => jwt.verify(token, JWT_SECRET);
