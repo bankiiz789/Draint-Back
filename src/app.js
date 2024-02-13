b1f7693ee4f99d6c3360dd5a7e7e3bc79a03888e
bankiiz789 <sukkarin7303@gmail.com>
2024-02-13 20:00:24 +0700

    push database 1st time

--- src/app.js
+++ src/app.js
@@ -2,13 +2,15 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const notFound = require("./middleware/not-found");
+const error = require("./middleware/error");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use(notFound());
+app.use(notFound);
+app.use(error);
 //Server
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
